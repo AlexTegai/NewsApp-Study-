392ed8668a177127706f7f1ab71e37c9bdf8df87
Aleksandr Tegai <shura.tegay@mail.ru>
2022-05-27 11:28:31 +0300

    Lesson-3

---
+++ app/src/main/java/ru/alte/newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package ru.alte.newsapp.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/ru/alte/newsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package ru.alte.newsapp.utils
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "ad0decb5cbb84d04b499a3990b9a5cd6"
+    }
+
+}
